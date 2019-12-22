645947bb1b17b89408d202ee16e508e240013f01
Donald Wu <yeukfei02@gmail.com>
2019-12-22 11:41:52 +0800

    add-more-routes-#3: create more routes

--- api/controller/restaurant.js
+++ api/controller/restaurant.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 
 const Restaurant = require('../model/restaurant');
 
-module.exports.getAllRestaurants = (req, res) => {
+module.exports.getAllRestaurantsByLatLong = (req, res) => {
   axios.get(`${process.env.YELP_HOST}/businesses/search`, {
     params: {
       term: req.query.food,
@@ -17,11 +17,139 @@ module.exports.getAllRestaurants = (req, res) => {
   })
     .then((result) => {
       res.status(200).json({
-        message: 'Get all restaurants!',
+        message: 'Get all restaurants by lat long!',
         restaurants: result.data
       });
     })
     .catch((error) => {
+      console.log("error = ", error);
+      res.status(404).json({
+        message: 'Not found'
+      });
+    });
+}
+
+module.exports.getAllRestaurantsByLocation = (req, res) => {
+  axios.get(`${process.env.YELP_HOST}/businesses/search`, {
+    params: {
+      term: req.query.food,
+      location: req.query.location
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: 'Get all restaurants by location!',
+        restaurants: result.data
+      });
+    })
+    .catch((error) => {
+      console.log("error = ", error);
+      res.status(404).json({
+        message: 'Not found'
+      });
+    });
+}
+
+module.exports.getRestaurantByPhone = (req, res) => {
+  axios.get(`${process.env.YELP_HOST}/businesses/search/phone`, {
+    params: {
+      phone: req.query.phone
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: 'Get restaurant by phone!',
+        restaurant: result.data
+      });
+    })
+    .catch((error) => {
+      console.log("error = ", error);
+      res.status(404).json({
+        message: 'Not found'
+      });
+    });
+}
+
+module.exports.getRestaurantDetailsById = (req, res) => {
+  axios.get(`${process.env.YELP_HOST}/businesses/${req.params.id}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: 'Get restaurant details by id!',
+        restaurantDetails: result.data
+      });
+    })
+    .catch((error) => {
+      console.log("error = ", error);
+      res.status(404).json({
+        message: 'Not found'
+      });
+    });
+}
+
+module.exports.getRestaurantDetailsReviewById = (req, res) => {
+  axios.get(`${process.env.YELP_HOST}/businesses/${req.params.id}/reviews`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: 'Get restaurant details review by id!',
+        restaurantDetailsReview: result.data
+      });
+    })
+    .catch((error) => {
+      console.log("error = ", error);
+      res.status(404).json({
+        message: 'Not found'
+      });
+    });
+}
+
+module.exports.getCategories = (req, res) => {
+  axios.get(`${process.env.YELP_HOST}/categories`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: 'Get categories!',
+        categories: result.data
+      });
+    })
+    .catch((error) => {
+      console.log("error = ", error);
+      res.status(404).json({
+        message: 'Not found'
+      });
+    });
+}
+
+module.exports.getCategoryByAlias = (req, res) => {
+  axios.get(`${process.env.YELP_HOST}/categories/${req.params.alias}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API_KEY}`
+    }
+  })
+    .then((result) => {
+      res.status(200).json({
+        message: 'Get category by alias!',
+        category: result.data
+      });
+    })
+    .catch((error) => {
+      console.log("error = ", error);
       res.status(404).json({
         message: 'Not found'
       });
--- api/routes/restaurant.js
+++ api/routes/restaurant.js
@@ -4,7 +4,13 @@ const expressListRoutes = require('express-list-routes');
 
 const restaurantController = require('../controller/restaurant');
 
-router.get('/find-restaurants', restaurantController.getAllRestaurants);
+router.get('/find-restaurants-by-lat-long', restaurantController.getAllRestaurantsByLatLong);
+router.get('/find-restaurants-by-location', restaurantController.getAllRestaurantsByLocation);
+router.get('/find-restaurant-by-phone', restaurantController.getRestaurantByPhone);
+router.get('/get-restaurant-details/:id', restaurantController.getRestaurantDetailsById);
+router.get('/get-restaurant-details-review/:id', restaurantController.getRestaurantDetailsReviewById);
+router.get('/get-categories', restaurantController.getCategories);
+router.get('/get-category/:alias', restaurantController.getCategoryByAlias);
 
 expressListRoutes({ prefix: '/api/restaurant' }, 'RESTAURANT API:', router);
 
