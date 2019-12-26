a31e3c520e7a9e662588bdaa9ebb18269da03d85
Donald Wu <yeukfei02@gmail.com>
2019-12-26 21:15:35 +0800

    add-sentry-timber-for-node-logging-#25: add timber lib

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,17 @@ import * as compression from 'compression';
 import * as mongoose from 'mongoose';
 import * as requestIp from 'request-ip';
 import * as Sentry from '@sentry/node';
+import { Timber } from "@timberio/node";
 
 import * as env from 'dotenv';
 env.config();
 
+// sentry
 Sentry.init({ dsn: process.env.SENTRY_DSN });
+
+// timber
+const timber = new Timber(process.env.TIMBER_API_KEY, process.env.TIMBER_SOURCE_ID);
+timber.pipe(process.stdout);
 
 import restaurantRoutes from './routes/restaurant';
 import categoryRoutes from './routes/category';
