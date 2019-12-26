dcfa636b965bfa1113a99780ee8b19c9a30570ac
Donald Wu <yeukfei02@gmail.com>
2019-12-26 19:42:22 +0800

    fix-code-#24:  add moment-timezone for mongoose table

--- src/model/userConnectionDetails.ts
+++ src/model/userConnectionDetails.ts
@@ -1,4 +1,9 @@
 import * as mongoose from 'mongoose';
+import * as moment from 'moment';
+import * as momenttz from 'moment-timezone';
+
+const userTimezone = momenttz.tz.guess();
+const currentDateWithTimezone = moment.tz(moment().format(), userTimezone);
 
 const userConnectionDetailsSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
@@ -7,9 +12,9 @@ const userConnectionDetailsSchema = new mongoose.Schema({
   client: { type: mongoose.Schema.Types.Mixed, required: false },
   device: { type: mongoose.Schema.Types.Mixed, required: false },
   routeName: { type: String, required: false },
+  created_by: { type: Date, default: currentDateWithTimezone },
+  updated_by: { type: Date, default: currentDateWithTimezone }
 });
-
-userConnectionDetailsSchema.set('timestamps', true);
 
 const userConnectionDetailsModel = mongoose.model('UserConnectionDetails', userConnectionDetailsSchema);
 
