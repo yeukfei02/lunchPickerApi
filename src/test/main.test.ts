5e0b6af67fbc0da35669d2b3562ab9e81d1d3689
Donald Wu <yeukfei02@gmail.com>
2020-01-06 22:24:09 +0800

    add-test-case-#65: add restaurant and category test case

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,5 +1,41 @@
 
-test('test number', () => {
-  const result = 1 + 2;
-  expect(result).toBe(3);
+import * as restaurant from './restaurant';
+import * as category from './category';
+
+describe('main.test', () => {
+  describe('restaurant', () => {
+    test('findLocationTextByLatLong', () => {
+      restaurant.findLocationTextByLatLong();
+    });
+
+    test('getAllRestaurantsByLatLong', () => {
+      restaurant.getAllRestaurantsByLatLong();
+    });
+
+    test('getAllRestaurantsByLocation', () => {
+      restaurant.getAllRestaurantsByLocation();
+    });
+
+    test('getRestaurantByPhone', () => {
+      restaurant.getRestaurantByPhone();
+    });
+
+    test('getRestaurantDetailsById', () => {
+      restaurant.getRestaurantDetailsById();
+    });
+
+    test('getRestaurantDetailsReviewById', () => {
+      restaurant.getRestaurantDetailsReviewById();
+    });
+  });
+
+  describe('category', () => {
+    test('getCategoies', () => {
+      category.getCategoies();
+    });
+
+    test('getCategoryByAlias', () => {
+      category.getCategoryByAlias();
+    });
+  });
 });
---
+++ src/test/restaurant.ts
@@ -0,0 +1,118 @@
+import * as _ from 'lodash';
+const axios = require('axios');
+
+const ROOT_URL = "https://lunch-picker-api.herokuapp.com/api";
+
+export const findLocationTextByLatLong = async () => {
+  const result = await axios.get(
+    `${ROOT_URL}/restaurant/find-location-text-by-lat-long`,
+    {
+      params: {
+        latitude: 1.3104729000000002,
+        longitude: 103.8577962
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  if (!_.isEmpty(result.data)) {
+    expect(result.data).toBeDefined();
+    expect(result.data.location).toBeDefined();
+  }
+}
+
+export const getAllRestaurantsByLatLong = async () => {
+  const result = await axios.get(
+    `${ROOT_URL}/restaurant/find-restaurants-by-lat-long`,
+    {
+      params: {
+        term: 'food',
+        latitude: 1.309160,
+        longitude: 103.852130
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  if (!_.isEmpty(result.data)) {
+    expect(result.data).toBeDefined();
+    expect(result.data.restaurants).toBeDefined();
+    expect(result.data.restaurants.businesses).toBeDefined();
+  }
+}
+
+export const getAllRestaurantsByLocation = async () => {
+  const result = await axios.get(
+    `${ROOT_URL}/restaurant/find-restaurants-by-location`,
+    {
+      params: {
+        term: 'food',
+        location: 'hong kong central'
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  if (!_.isEmpty(result.data)) {
+    expect(result.data).toBeDefined();
+    expect(result.data.restaurants).toBeDefined();
+    expect(result.data.restaurants.businesses).toBeDefined();
+  }
+}
+
+export const getRestaurantByPhone = async () => {
+  const result = await axios.get(
+    `${ROOT_URL}/restaurant/find-restaurant-by-phone`,
+    {
+      params: {
+        phone: '+85227881226'
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  if (!_.isEmpty(result.data)) {
+    expect(result.data).toBeDefined();
+    expect(result.data.restaurant).toBeDefined();
+  }
+}
+
+export const getRestaurantDetailsById = async () => {
+  const result = await axios.get(
+    `${ROOT_URL}/restaurant/get-restaurant-details/X7b2izv3qhklnCDjoF37tA`,
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  if (!_.isEmpty(result.data)) {
+    expect(result.data).toBeDefined();
+    expect(result.data.restaurantDetails).toBeDefined();
+  }
+}
+
+export const getRestaurantDetailsReviewById = async () => {
+  const result = await axios.get(
+    `${ROOT_URL}/restaurant/get-restaurant-details-review/X7b2izv3qhklnCDjoF37tA`,
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  if (!_.isEmpty(result.data)) {
+    expect(result.data).toBeDefined();
+    expect(result.data.restaurantDetailsReview).toBeDefined();
+  }
+}
