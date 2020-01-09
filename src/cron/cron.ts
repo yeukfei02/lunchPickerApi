79589f23c5900eb4a730b8a0bd3db0d27eb47b6a
Donald Wu <yeukfei02@gmail.com>
2020-01-10 00:48:02 +0800

    fix-code: remove firebase admin json and update env

--- src/cron/cron.ts
+++ src/cron/cron.ts
@@ -4,9 +4,21 @@ import * as _ from 'lodash';
 
 import { log } from '../common/common';
 
-const serviceAccount = require("../../lunchpicker-2232b-firebase-adminsdk-sxq0e-c802a6e8a6.json");
+const serviceAccount = {
+  "type": process.env.FIREBASE_ADMIN_TYPE,
+  "project_id": process.env.FIREBASE_ADMIN_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_ADMIN_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_ADMIN_AUTH_URI,
+  "token_uri": process.env.FIREBASE_ADMIN_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+};
+const serviceAccountStr = JSON.stringify(serviceAccount);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(serviceAccountStr)),
   projectId: process.env.FIREBASE_PROJECT_ID
 }, 'cronFirebaseAdmin');
 
