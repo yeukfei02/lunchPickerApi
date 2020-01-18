6085a73dbcc03ecfd8930305a2a544d609a621f9
Donald Wu <yeukfei02@gmail.com>
2020-01-18 17:18:29 +0800

    fix-favourites-model-#100: fix favourites route and model

--- src/model/favourites.ts
+++ src/model/favourites.ts
@@ -7,7 +7,7 @@ const currentDateWithTimezone = moment.tz(moment().format(), userTimezone);
 
 const favouritesSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  current_token: { type: String, ref: 'firebaseDetails', required: true },
+  ip: { type: String, required: true },
   item: { type: mongoose.Schema.Types.Mixed, required: true },
   created_by: { type: Date, default: currentDateWithTimezone },
   updated_by: { type: Date, default: currentDateWithTimezone }
